79f226d0549ef5c7da6934ba04c33939135cdf89
Hemanth KR <krh7799@gmail.com>
2023-11-11 12:37:50 +0530

    ConditionalRendering_Login

--- client/src/Pages/Nav.jsx
+++ client/src/Pages/Nav.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Styles/Nav.css";
-
+import profile from "C:\\Users\\Hemanth KR\\OneDrive\\Desktop\\Practice\\client\\src\\images\\1.png";
 function Nav() {
   return (
     <>
       <div className="main">
         <h3 className="title">Coursera</h3>
+        {/* if ()
+        {
+          <div className="buttons">
+            <Link to="/signin">
+              <button className="b1">LOG IN</button>
+            </Link>
+
+            <Link to="/signup">
+              <button className="b2">JOIN FOR FREE</button>
+            </Link>
+          </div>
+        }{" "}
+        else{" "}
+        { */}
         <div className="buttons">
-          <Link to="/signin">
-            <button className="b1">LOG IN</button>
+          <Link to="/profile">
+            <button className="b1 bimg">
+              <img src={profile} alt="" className="profileImg" />
+            </button>
           </Link>
 
-          <Link to="/signup">
+          {/* <Link to="/signup">
             <button className="b2">JOIN FOR FREE</button>
-          </Link>
+          </Link> */}
         </div>
+        {/* } */}
       </div>
     </>
   );
